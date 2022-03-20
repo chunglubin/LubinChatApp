8123ec9ad8038e0aee7c97ab7a930a3b98b3d166
User <lubinfoever2020@gmail.com>
2022-03-20 23:23:56 +0800

    access the video.

--- app/src/main/java/com/lubin/lubinchatapp/RoomFragment.kt
+++ app/src/main/java/com/lubin/lubinchatapp/RoomFragment.kt
@@ -1,6 +1,7 @@
 package com.lubin.lubinchatapp
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Binder
 import android.os.Bundle
 import android.util.Log
@@ -48,5 +49,6 @@ class RoomFragment : Fragment() {
         Log.d(TAG, "room:${room?.stream_title}")
         Log.d(TAG, "room:${room?.stream_id}")
         Log.d(TAG, "room:${room?.start_time}")
+        binding.video.setVideoURI((Uri.parse("https://player.vimeo.com/video/653928650")))
     }
 }
